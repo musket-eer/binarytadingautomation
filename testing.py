2f9f5008c341f5eb814a91c84b2ea5d7d1f06396
musket-eer <russel.docs@gmail.com>
2021-10-11 00:11:21 -0700

    fixed the bugs in the while loops

--- testing.py
+++ testing.py
@@ -11,42 +11,57 @@ import tradeauto
 time.sleep(5)
 
 intitial_bal = tradeauto.check_balance()
-bal1 = float(tradeauto.check_balance())
-bal2=float(tradeauto.check_balance())
 
+bal1 = intitial_bal
+bal2 = 0
 trades = 0
-compensator = 1
+compensator = 0
 while trades <= 80:
-    tradeauto.stake_amount(0.01 * bal1 * compensator)
+    if tradeauto.on_profit(bal1, bal2):
+        compensator = 0
+
+    tradeauto.stake_amount(0.01 * bal1 * 2 ** compensator)
     tradeauto.execute_trade_up()
     trades += 1
-    time.sleep(61)
+    time.sleep(62)
     bal2 = tradeauto.check_balance()
+    print(trades, bal1, bal2, tradeauto.on_profit(bal1, bal2))
+
     while tradeauto.on_profit(bal1, bal2):
-        tradeauto.reset_stake()
+        compensator = 0
+        tradeauto.stake_amount(0.01 * bal2)
         tradeauto.execute_trade_up()
         trades += 1
-        time.sleep(61)
+        time.sleep(62)
         bal1 = bal2
         bal2 = tradeauto.check_balance()
+        print(trades, bal1, bal2, tradeauto.on_profit(bal1, bal2))
 
-    compensator *= 2
-    tradeauto.stake_amount(0.01 * bal1 * compensator)
+
+    compensator += 1
+
+    tradeauto.stake_amount(0.01 * bal1 * 2 ** compensator)
     tradeauto.execute_trade_down()
     trades += 1
-    time.sleep(61)
+    time.sleep(62)
     bal1 = bal2
     bal2 = tradeauto.check_balance()
+    print(trades, bal1, bal2, tradeauto.on_profit(bal1, bal2))
 
     while tradeauto.on_profit(bal1, bal2):
-        tradeauto.reset_stake()
+        compensator = 0
+        tradeauto.stake_amount(0.01 * bal2)
         tradeauto.execute_trade_down()
         trades += 1
         time.sleep(62)
         bal1 = bal2
         bal2 = tradeauto.check_balance()
-
+        print(trades, bal1, bal2, tradeauto.on_profit(bal1, bal2))
+        
+        
+    compensator += 1
     trades  += 1
+
 
 print(trades)
 print(tradeauto.check_balance() - intitial_bal)
